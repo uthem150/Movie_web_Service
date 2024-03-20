36413d885c2c5e46e79b138770877f34d98a390f
uthem150 <uthem150@gmail.com>
2024-03-20 23:39:05 +0900

    1

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { useState, useEffect } from "react";
+
+function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("I run all the time");
+  useEffect(() => {
+    console.log("call the api");
+  }, []);
+  return (
+    <div>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
+    </div>
+  );
+}
+export default App;
