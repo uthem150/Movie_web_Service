91ce6a8ddac401bafab807297a383c2e547d19b1
uthem150 <uthem150@gmail.com>
2024-03-27 22:16:05 +0900

    useEffect를 이용하여, 변화를 감지하여 코드실행

--- src/App.js
+++ src/App.js
@@ -4,14 +4,29 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
-  console.log("i run all the time");
+  const onChange = (event) => setKeyword(event.target.value);
   useEffect(() => {
-    console.log("CALL THE API.....");
-  }, []);
+    console.log("I run only once.");
+  }, []); //지켜보는 것이 없기 때문에, 한번만 실행
+  useEffect(() => {
+    console.log("I run when 'keyword' changes. keyword is ", keyword);
+  }, [keyword]); //keyword가 변화할 때만, 코드를 실행
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
+  useEffect(() => {
+    console.log("I run when 'counter' & 'keyword' changes.");
+  }, [counter, keyword]);
   return (
     <div>
-      <input type="text" placeholder="Search here" />
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here"
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>click me</button>
     </div>
