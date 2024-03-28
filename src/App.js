986769172cd3e4e337ff21f12cce2866ec99a2f7
uthem150 <uthem150@gmail.com>
2024-03-28 22:30:27 +0900

    componet가 destory될 때 실행하는 cleanup function

--- src/App.js
+++ src/App.js
@@ -1,34 +1,33 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { useState, useEffect } from "react";
 
+function Hello() {
+  // function byeFn() {
+  //   console.log("destroyed");
+  // }
+
+  // function hiFn() {
+  //   console.log("created");
+  //   return byeFn; //component가 destroy될 때 뭔가 실행하는 cleanup function
+  // }
+  // useEffect(hiFn, []);
+  useEffect(() => {
+    console.log("hi");
+    return function () {
+      console.log("bye");
+    };
+  }, []);
+  return <h1>Hello</h1>;
+}
+
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  useEffect(() => {
-    console.log("I run only once.");
-  }, []); //지켜보는 것이 없기 때문에, 한번만 실행
-  useEffect(() => {
-    console.log("I run when 'keyword' changes. keyword is ", keyword);
-  }, [keyword]); //keyword가 변화할 때만, 코드를 실행
-  useEffect(() => {
-    console.log("I run when 'counter' changes.");
-  }, [counter]);
-  useEffect(() => {
-    console.log("I run when 'counter' & 'keyword' changes.");
-  }, [counter, keyword]);
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here"
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>{" "}
+      {/*showing하면 Hide를 보여주고, showing하지 않으면 Show보여줌*/}
     </div>
   );
 }
