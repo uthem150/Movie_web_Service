2042635a2ffa7970c040ead7b942c8d5242d8382
uthem150 <uthem150@gmail.com>
2024-03-29 22:11:32 +0900

    Todo List 1

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 
-function Hello() {
-  // function byeFn() {
-  //   console.log("destroyed");
-  // }
-
-  // function hiFn() {
-  //   console.log("created");
-  //   return byeFn; //component가 destroy될 때 뭔가 실행하는 cleanup function
-  // }
-  // useEffect(hiFn, []);
-  useEffect(() => {
-    console.log("hi");
-    return function () {
-      console.log("bye");
-    };
-  }, []);
-  return <h1>Hello</h1>;
-}
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault(); //form이 작동하는 것을 막기
+    if (toDo === "") {
+      //비어있으면 작동시키지 않음
+      return;
+    }
+    setToDo(""); //실행 후 input 내용 지워주기
+    setToDos((currentArray) => [toDo, ...currentArray]);
+  };
+  console.log(toDos);
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>{" "}
-      {/*showing하면 Hide를 보여주고, showing하지 않으면 Show보여줌*/}
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do"
+        ></input>
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }
