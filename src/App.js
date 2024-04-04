36c4ac939a1a84efd8b1c84c154bdb44cc26bb8a
uthem150 <uthem150@gmail.com>
2024-04-05 00:34:58 +0900

    react-router-dom을 이용한 routes설정

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import "./App.css";
-import Movie from "./components/Movie";
-import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/detail";
 
 function App() {
-  return null;
+  return (
+    <Router>
+      <Routes>
+        {/* 'element' prop을 사용하여 컴포넌트를 전달 */}
+        <Route path="/" element={<Home />} />
+        <Route path="/movie" element={<Detail />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,11 +1,14 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 function Movie({ coverImage, title, summary, genres }) {
   //부모로부터 위의 값들을 받아옴
   return (
     <div>
       <img src={coverImage} alt={title} />
-      <h2>{title}</h2>
+      <h2>
+        <Link to="/movie">{title}</Link>
+      </h2>
       <p>{summary}</p>
 
       <ul>{genres && genres.map((g) => <li key={g}>{g}</li>)}</ul>
@@ -15,7 +18,7 @@ function Movie({ coverImage, title, summary, genres }) {
 
 Movie.propTypes = {
   //Movie component의 type설정
-  coverImg: PropTypes.string.isRequired,
+  coverImage: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
   genres: PropTypes.arrayOf(PropTypes.string).isRequired, //장르는 string을 가진 array
